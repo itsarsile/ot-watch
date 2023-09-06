699c98c6877c9a0d2f4dc407f8c7abb8c0229132
Arsile <ajiprio@hotmail.com>
2023-09-06 23:11:27 +0700

    change force dynamic

--- app/(dashboard)/dashboard/opentable/api/attendance/route.ts
+++ app/(dashboard)/dashboard/opentable/api/attendance/route.ts
@@ -10,6 +10,8 @@ import { sql } from "drizzle-orm";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
   try {
     const session = await getServerSession(authOptions);
