bac0071f5f5f56e9db8d0a80c605dfa4941eea4b
Arsile <ajiprio@hotmail.com>
2023-09-06 23:03:44 +0700

    fixed: client data fetching

--- app/(dashboard)/dashboard/opentable/page.tsx
+++ app/(dashboard)/dashboard/opentable/page.tsx
@@ -1,40 +1,9 @@
-import React from "react";
 import { MapsComponents } from "./OpenTableMaps";
-import { cookies } from "next/headers";
-
-async function getCookieData() {
-  const cookieData = cookies().toString()
-  return new Promise((resolve) =>
-    setTimeout(() => {
-      resolve(cookieData)
-    }, 1000)
-  )
-}
-
-async function getAttendanceData() {
-  const cookies = await getCookieData()
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/dashboard/opentable/api/attendance`,
-    {
-      //@ts-ignore chill buddy
-      headers: {
-        Cookie: cookies,
-      },
-      cache: "no-store"
-    }
-  );
-
-  if (res.ok) {
-    return res.json();
-  }
-  console.error("Error fetching attendances");
-}
 
 async function OpenTablePage() {
-  const attendanceData = await getAttendanceData();
   return (
     <div>
-      <MapsComponents attendanceData={attendanceData}/>
+      <MapsComponents />
     </div>
   );
 }
