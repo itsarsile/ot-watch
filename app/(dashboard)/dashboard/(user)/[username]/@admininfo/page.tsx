036d9b213bcd847cab24414ff5c90c7cb03b061b
Arsile <ajiprio@hotmail.com>
2023-09-16 11:39:32 +0700

    fix: add force dynamic

--- app/(dashboard)/dashboard/(user)/[username]/@admininfo/page.tsx
+++ app/(dashboard)/dashboard/(user)/[username]/@admininfo/page.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ProspekView from "./ProspekView";
 import VisitorNeedsView from "./VisitorNeedsView";
-import VisitorReports from "../@userinfo/VisitorReports";
 import ReportList from "../@userinfo/ReportList";
+
+export const dynamic = 'force-dynamic'
+
 function AdminInfoPage() {
   return (
     <div className="grid lg:grid-cols-6 gap-5">
