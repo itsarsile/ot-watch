82e2675f223d50a67c281d02319de8cd4c12b8f6
Arsile <ajiprio@hotmail.com>
2023-09-16 10:30:52 +0700

    force dynamic

--- app/(dashboard)/dashboard/(user)/[username]/@admininfo/VisitorNeedsView.tsx
+++ app/(dashboard)/dashboard/(user)/[username]/@admininfo/VisitorNeedsView.tsx
@@ -17,9 +17,14 @@ import {
   LinearScale,
   Tooltip,
 } from "chart.js";
+import { DynamicOptions } from "next/dynamic";
 
 import { Bar } from "react-chartjs-2";
 import useSWR from "swr";
+
+export const dynamic: DynamicOptions = {
+  ssr: true,
+}
 
 ChartJS.register(CategoryScale, Legend, Tooltip, LinearScale, BarElement);
 
